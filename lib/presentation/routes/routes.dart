0430265fcd1bfefe09f58fa876477bb0ccf822f4
AhmadNaufal2525 <ahmadnaufal588@gmail.com>
2023-11-06 22:27:37 +0700

    add routes

---
+++ lib/presentation/routes/routes.dart
@@ -0,0 +1,13 @@
+class Routes {
+  static get splashScreen => '/';
+  static get loginScreen => '/login';
+  static get registerScreen => '/register';
+  static get homeScreen => '/home';
+  static get listAllCourseScreen => '/courseList';
+  static get exerciseListScreen => '/exerciseList';
+  static get exerciseScreen => '/exercise';
+  static get resultScreen => '/result';
+  static get profileScreen => '/profile';
+  static get profileEditScreen => '/profileEdit';
+  static get diskusiScreen => '/diskusi';
+}
